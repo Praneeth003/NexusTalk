53eea37378d43c7a7079e75322aac8a4c61fa634
Praneeth003 <ch.krishnapraneeth@gmail.com>
2023-11-11 13:40:54 -0500

    set up server

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req,res) =>{
+    res.send("<h1>Yeah!</h1>");
+});
+
+app.listen(port, () =>{
+    console.log(`Server is running at port ${port}`);
+})
