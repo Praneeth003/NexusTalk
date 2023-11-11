379cabce940da941f3f6bd3bf7e81b288169a0cd
Praneeth003 <ch.krishnapraneeth@gmail.com>
2023-11-11 14:22:28 -0500

    set up env file

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
 
 const app = express();
-const port = 3000;
+dotenv.config();
 
 app.get("/", (req,res) =>{
     res.send("<h1>Yeah!</h1>");
 });
 
+const port = process.env.PORT ;
 app.listen(port, () =>{
     console.log(`Server is running at port ${port}`);
 })
