ac15adba7b8b3abc9deedcbe4243327521e045be
Praneeth003 <ch.krishnapraneeth@gmail.com>
2023-11-14 17:58:22 -0500

    add title in the home page

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import { Container, Box, Text} from "@chakra-ui/react";
 
 function Home(){
     return(
-
-        <h1>Home Page</h1>
+        <Container maxW = 'xl' centerContent>
+            <Box
+            d = "flex"
+            justifyContent="center"
+            p = {3}
+            bg = "rgba(176, 217, 177)"
+            w = "100%"
+            m = "40px 0 15px 0"
+            borderRadius = "1g"
+            borderWidth = "1px"
+            >
+                <Text fontSize="4xl" color = "black" textAlign="center">NexusTalk</Text>
+            </Box>
+        </Container>
     );
 }
 
