d8d97243c78ba29ec17ae3c3bd631decee7cb713
Praneeth003 <ch.krishnapraneeth@gmail.com>
2023-11-15 16:46:52 -0500

    create SignIn component

--- frontend/src/components/Authentication/SignIn.jsx
+++ frontend/src/components/Authentication/SignIn.jsx
@@ -1,10 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import {FormControl, FormLabel, Input, Button, InputRightElement, InputGroup} from "@chakra-ui/react";
 
 function SignIn(){
+    const [state, setState] = useState({
+        email: "",
+        password: "",
+
+    })
+
+    const [showPassword, setShowPassword] = useState(false);
+
+    function recordInput(event){
+        const {name, value} = event.target;
+        setState((prevValue =>{
+            return{
+            ...prevValue,
+            [name] : value
+        };})
+        );
+    }
+
+    function handleClick(){
+        setShowPassword(!showPassword);
+    }
+
+    function onSubmit(event){
+        //Implement later
+    }
+        
+
     return(
-        <div>
+        <vStack>
+            
+            <FormControl isRequired>
+                <FormLabel>Email</FormLabel>
+                    <Input name ="email" placeholder = "Enter Your Email Address" value = {state.email} onChange = {recordInput} /> 
+            </FormControl>
+
+            <FormControl isRequired>
+                <FormLabel>Password</FormLabel>
+                    <InputGroup>
+                    <Input name ="password" placeholder = "Enter Password" value = {state.password} onChange = {recordInput} 
+                        type={showPassword ? "text" : "password"}
+                    /> 
+                    <InputRightElement>
+                        <Button onClick = {handleClick} size ="sm" colorScheme='gray' variant='outline' >{showPassword ? "Hide" : "Show"}</Button>
+                    </InputRightElement>  
+                    </InputGroup>
+            </FormControl>
+
+            <Button colorScheme = 'blue' width = "100%" onClick = {onSubmit} mt = {5} >
             Sign In
-        </div>
+            </Button>
+
+        </vStack>
     );
 }
 
