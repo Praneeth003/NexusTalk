5d5a4fef2af0a60640895ab550e2abecdcb28ef0
surajkrovvidi <surajkrovvidi@gmail.com>
2023-12-04 17:20:25 -0500

    access user and setUser using contextApi

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -1,30 +1,15 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios"; 
+import {chatState} from "../Context/ChatProvider";
 
 axios.defaults.baseURL = 'http://localhost:4000';
+
 function Chat(){
-    const [chatData, setChatData] = useState([]);
-    
-    async function fetchChatData() {
-        try {
-        const response = await axios.get("/api/chat");
-        console.log(response.data);
-        setChatData(response.data);
-        } catch (error) {
-        console.error("Error fetching chat data:", error);
-        }
-    }
-    console.log(chatData);
-    useEffect(() => {
-    fetchChatData();
-    }, []);
-  
+    const {user} = chatState();
+
    return (
-    <div>
-    {chatData.map(i => {
-        return <div key = {i._id} > {i.chatName} </div>}
-    )
-    }
+    <div style={{width: "100%"}}>
+
     </div>
 );
 }
