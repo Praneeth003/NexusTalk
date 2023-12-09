ef0ed6721dd09c939d32a4de6136c913b899a57c
praneeth003 <ch.krishnapraneeth@gmail.com>
2023-12-08 23:08:16 -0500

    modal: create group name and add users functionality

--- frontend/src/components/Other/GroupChatModal.jsx
+++ frontend/src/components/Other/GroupChatModal.jsx
@@ -9,11 +9,60 @@ import {
   ModalFooter,
   Button, 
   useDisclosure,
+  FormControl,
 } from '@chakra-ui/react';
+import {useState} from 'react';
+import {useToast} from '@chakra-ui/react';
+import {ChatState} from '../../Context/ChatProvider';
+import {Input} from '@chakra-ui/react';
+import axios from 'axios';
+import UserListItem from './UserListItem';
 
 const GroupChatModal = ({children}) => {
   // Add your component logic here
   const {isOpen, onOpen, onClose} = useDisclosure();
+  const [groupName, setGroupName] = useState('');
+  const [selectedUsers, setSelectedUsers] = useState([]);
+  const [search, setSearch] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const toast = useToast();
+  const {user, chatList, setChatList} = ChatState();
+  
+
+ const handleSearch =  async (val) => {
+    setSearch(val);
+    if(!val){
+        return;
+    }
+    try{
+        const config = {
+            headers: {
+                Authorization: `Bearer ${user.token}`,
+            },
+            };
+            const {data} = await axios.get(`/api/user?search=${search}`, config);
+            console.log(data);
+            setSearchResults(data);
+    }catch(error){
+        toast({
+            title: 'Something went wrong',
+            description: 'Unable to search',
+            status: 'error',
+            duration: 3000,
+            isClosable: true,
+            position: 'bottom-left',
+        });
+    }
+
+    // Add your search logic here
+    };
+ 
+    const handleSubmit = async () => {
+    };
+
+    const handleSelect = (i) => {
+        setSelectedUsers([...selectedUsers, i]);
+    }
 
   return (
     <>
@@ -21,17 +70,42 @@ const GroupChatModal = ({children}) => {
       <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
+          <ModalHeader
+            fontFamily='Work Sans'
+            fontWeight='bold'
+            fontSize='2xl'
+            textAlign='center'
+            color='blue.500'
+          >Create Group</ModalHeader>
           <ModalCloseButton />
-          <ModalBody>
-            
+          <ModalBody
+            d = 'flex'
+            flexDir = 'column'
+            justifyContent = 'center'
+            alignItems = 'center'
+          >
+          <FormControl>
+          <Input placeholder="Group Name" value={groupName} onChange={(e) => setGroupName(e.target.value)} />  
+          </FormControl>
+
+        <FormControl>
+        <Input placeholder="search for users" value={search} onChange={(e) => handleSearch(e.target.value)} />
+        </FormControl>
+          
+          {searchResults.map((i) => (
+            <UserListItem
+              key={i._id}
+              i={i}
+              handleFunction={() => handleSelect(i)}
+            />
+          ))}
+
           </ModalBody>
 
           <ModalFooter>
-            <Button colorScheme='blue' mr={3} onClick={onClose}>
-              Close
+            <Button colorScheme='blue' mr={3} onClick={handleSubmit}>
+                Create
             </Button>
-            <Button variant='ghost'>Secondary Action</Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
