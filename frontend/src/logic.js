86b23cab70029581c2691d94def62af12fc6d836
praneeth003 <ch.krishnapraneeth@gmail.com>
2023-12-21 12:16:59 -0500

    finish chatbox ui

--- frontend/src/logic.js
+++ frontend/src/logic.js
@@ -1,24 +1,47 @@
+export const isSameSenderMargin = (messages, m, i, userId) => {
+  // console.log(i === messages.length - 1);
 
-function getSender(loggedInUser, users) {
- return users[0]._id === loggedInUser._id ? users[1].name : users[0].name;
-}
+  if (
+    i < messages.length - 1 &&
+    messages[i + 1].sender._id === m.sender._id &&
+    messages[i].sender._id !== userId
+  )
+    return 33;
+  else if (
+    (i < messages.length - 1 &&
+      messages[i + 1].sender._id !== m.sender._id &&
+      messages[i].sender._id !== userId) ||
+    (i === messages.length - 1 && messages[i].sender._id !== userId)
+  )
+    return 0;
+  else return "auto";
+};
 
-function getFullSender(loggedInUser, users) {
- return users[0]._id === loggedInUser._id ? users[1] : users[0];
-}
+export const isSameSender = (messages, m, i, userId) => {
+  return (
+    i < messages.length - 1 &&
+    (messages[i + 1].sender._id !== m.sender._id ||
+      messages[i + 1].sender._id === undefined) &&
+    messages[i].sender._id !== userId
+  );
+};
 
+export const isLastMessage = (messages, i, userId) => {
+  return (
+    i === messages.length - 1 &&
+    messages[messages.length - 1].sender._id !== userId &&
+    messages[messages.length - 1].sender._id
+  );
+};
 
-function isLastMessage(messages, i, userId){
-    return  (
-        i === messages.length - 1 && messages[messages.length - 1].sender._id !== userId && messages[messages.length - 1].sender_id 
-    );
-}
+export const isSameUser = (messages, m, i) => {
+  return i > 0 && messages[i - 1].sender._id === m.sender._id;
+};
 
-function isSameSender(messages, m, i, userId){
-    return(
-        i < messages.length - 1 && (messages[i + 1].sender._id !== m.sender_id || messages[i + 1].sender._id === undefined)
-        && messages[i].sender._id !== userId
-    );
-}
+export const getSender = (loggedUser, users) => {
+  return users[0]?._id === loggedUser?._id ? users[1].name : users[0].name;
+};
 
-export { getSender, getFullSender, isLastMessage, isSameSender };
+export const getFullSender = (loggedUser, users) => {
+  return users[0]._id === loggedUser._id ? users[1] : users[0];
+};
